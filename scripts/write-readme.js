77bd020ceb2736757a1ed74357bb1df8b74b3963
Rene Pot <rene@topener.nl>
2023-01-25 09:39:59 +0100

    update script to support mastodon

--- scripts/write-readme.js
+++ scripts/write-readme.js
@@ -1,7 +1,7 @@
-'use strict;'
-const fs = require('fs');
-const appRoot = require('app-root-path');
-let path = require('path');
+"use strict;";
+const fs = require("fs");
+const appRoot = require("app-root-path");
+let path = require("path");
 
 //Get the data
 path = path.parse(appRoot.path);
@@ -15,10 +15,27 @@ json.people.sort(function (a, b) {
 });
 
 //Generate with proper formating
-const peopleList = json.people.map(people =>
-	`- **[${people.name}](${people.scheduling}) (${(people.linkedin ? "[LinkedIn](" + people.linkedin + "), " : "")}${(people.twitter ? "[Twitter](" + people.twitter + ")" : "")}), ${people.title} at ${people.company}:** ${people.topics.join(', ')}`
-).join('\r\n');
+const peopleList = json.people
+    .map(
+        (person) => {
+            let socials = [];
+            ['linkedin', 'twitter', 'mastodon'].forEach(social => {
+                if (person.hasOwnProperty(social) && person[social].length > 0) {
+                    const socialName = social === 'linkedin' ? 'LinkedIn' : social.charAt(0).toUpperCase() + social.slice(1);
+                    socials.push(`[${socialName}](${person[social]})`);
+                }
+            });
+
+            return `- **[${person.name}](${person.scheduling}) (${socials.join(', ')}), ${
+                person.title
+            } at ${person.company}:** ${person.topics.join(", ")}`
+        }
+    )
+    .join("\r\n");
 
 //Write README.md
-const template = fs.readFileSync(`${appRoot}/README-TEMPLATE.md`, 'utf8');
-fs.writeFileSync(`${parentFolderPath}/README.md`, template.replace('PLACEHOLDER', peopleList));
+const template = fs.readFileSync(`${appRoot}/README-TEMPLATE.md`, "utf8");
+fs.writeFileSync(
+    `${parentFolderPath}/README.md`,
+    template.replace("PLACEHOLDER", peopleList)
+);
