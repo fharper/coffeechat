b0f89f72f25252bc1372cf2b490fbd8069f8fc63
Simon Emms <simon@simonemms.com>
2025-01-19 19:53:18 +0000

    feat(app): add site description

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -6,6 +6,17 @@
 </script>
 
 <div class="section">
+  <div class="container is-size-3">
+    List of awesome people offering their time 🎉 <strong>for free</strong> 🎉
+    to have a
+    <a
+      href="https://www.indeed.com/career-advice/career-development/coffee-chat"
+      target="_blank">coffee chat</a
+    > with others about different topics, mostly in a mentorship kind of way.
+  </div>
+</div>
+
+<div class="section">
   <div class="container">
     <div class="columns is-multiline">
       {#each data.people as person}
