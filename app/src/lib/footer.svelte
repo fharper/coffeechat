a1509d755a16cd9d0305f307a3ff762c218fc42b
Simon Emms <simon@simonemms.com>
2025-01-19 20:01:42 +0000

    feat(app): add footer

---
+++ app/src/lib/footer.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  const now = new Date();
+</script>
+
+<footer class="footer">
+  <div class="content has-text-centered">
+    <p>
+      The idea of a coffee chat resource list is coming from <a
+        href="https://www.linkedin.com/in/julienlengrand/"
+        target="_blank"
+      >
+        Julien Lengrand-Lambert
+      </a>.
+    </p>
+    <p>
+      Licensed under <a href="https://unlicense.org/" target="_blank">
+        The Unlicense
+      </a>
+    </p>
+    <p>
+      &copy; 2022 - {now.getFullYear()}
+      <a href="https://fred.dev/" target="_blank">Fred Harper</a>
+      and
+      <a
+        href="https://github.com/fharper/coffeechat/graphs/contributors"
+        target="_blank"
+      >
+        contributors
+      </a>
+    </p>
+  </div>
+</footer>
